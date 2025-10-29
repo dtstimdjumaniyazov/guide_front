771bd1d2a9ae5bbb693dcaa5aed3682087ee4639
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2025-10-30 01:29:27 +0500

    update qr.tsx

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -7,7 +7,7 @@ export default function QRSection() {
     <div className="flex flex-col items-center gap-2">
       <p className="text-lg font-semibold">{t("QR")}</p>
       <QRCodeCanvas
-        value="https://uchrejdeniya.uz"
+        value="https://child-guide.co.uz/"
         size={150}
         bgColor="#ffffff"
         fgColor="#000000"
@@ -15,12 +15,12 @@ export default function QRSection() {
         includeMargin={true}
       />
       <a
-        href="https://uchrejdeniya.uz"
+        href="https://child-guide.co.uz/"
         className="text-blue-600 hover:underline mt-2"
         target="_blank"
         rel="noopener noreferrer"
       >
-        https://uchrejdeniya.uz
+        https://www.child-guide.co.uz/
       </a>
     </div>
   );
